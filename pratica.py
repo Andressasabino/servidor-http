bdff37cb3f0d130804c577da92ab6b255a358db2
Andressa_SabinoIFCE <84898051+Andressasabino@users.noreply.github.com>
2022-09-19 19:17:42 -0300

    Create pratica.py

---
+++ pratica.py
@@ -0,0 +1,22 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+PORT = 5000
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        message = "Flamengo nao e time e nem selecao"
+        self.wfile.write(bytes(message, "utf8"))
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        message = "Hello, World! Here is a POST response"
+        self.wfile.write(bytes(message, "utf8"))
+
+with HTTPServer(('', 5000), handler) as server:
+    server.serve_forever()
